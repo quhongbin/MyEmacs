2b3b31f1837ada8914e724f544c89aa2aba636bc
Hongbin Qu <2818777520@qq.com>
2026-04-26 18:42:34 +0800

    change theme with doom-gruvbox

--- lisp/themes.el
+++ lisp/themes.el
@@ -1,10 +1,12 @@
 ;;(unless (package-installed-p 'gruvbox-theme)
 ;;  (package-refresh-contents)
 ;;  (package-install 'gruvbox-theme))
-(use-package gruvbox-theme
-  :ensure t
-  :config
-  (load-theme 'gruvbox t))
-
+(if (package-installed-p 'doom-themes)
+     (load-theme 'doom-gruvbox t)
+  (use-package doom-themes
+    :ensure t
+    :config
+    (load-theme 'doom-gruvbox t))
+  )
 ;;load themes
 (provide 'themes)
