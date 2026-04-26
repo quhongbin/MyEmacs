45bbf8fb087e1128152363ce68a9ce18ef2dd880
Hongbin Qu <2818777520@qq.com>
2026-04-26 18:42:14 +0800

    add eaf plugin framework

--- init.el
+++ init.el
@@ -1,8 +1,34 @@
-;;(require 'use-package)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/") t)
 
-(add-to-list 'load-path "~/.config/emacs/lisp")
+(setq eaf-python-command "/opt/eaf-venv/bin/python3")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/")
+(require 'eaf)
+(require 'eaf-mind-elixir)
+(require 'eaf-music-player)
+(require 'eaf-rss-reader)
+(require 'eaf-terminal)
+(require 'eaf-vue-demo)
+(require 'eaf-vue-tailwindcss)
+(require 'eaf-mindmap)
+(require 'eaf-image-viewer)
+;;(require 'eaf-video-editor)
+(require 'eaf-video-player)
+(require 'eaf-org-previewer)
+(require 'eaf-markdown-previewer)
+(require 'eaf-jupyter)
+(require 'eaf-markmap)
+(require 'eaf-map)
+(require 'eaf-pdf-viewer)
+(require 'eaf-git)
+(require 'eaf-airshare)
+(require 'eaf-pyqterminal)
+(require 'eaf-system-monitor)
+(require 'eaf-file-manager)
+(require 'eaf-browser)
+(require 'eaf-file-sender)
+(add-to-list 'load-path "~/.emacs.d/lisp")
+(require 'editor)
 (require 'themes)
 (require 'lsp-config)
 (require 'org-config)
@@ -13,14 +39,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326"
+   '("b7a09eb77a1e9b98cafba8ef1bd58871f91958538f6671b22976ea38c2580755"
+     "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326"
      "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1"
      "e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0"
      "10e330880269244ae45ae9e02fe6f55766da9e15036e7c7f07d7ce228195deb5"
      default))
  '(package-selected-packages
-   '(autothemer company dash doom-themes f gruvbox-theme ht lsp-mode lv
-		markdown-mode modus-themes s spinner)))
+   '(company doom-themes gruvbox-theme lsp-mode modus-themes
+	     terminal-here vterm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
