5b607711f1d5ac644549a0c94a1ffc6ff447cfec
arch-home-hp <thinkbeforedo1124@gmail.com>
2026-09-01 23:13:15 +0800

    merging conflict

--- init.el
+++ init.el
@@ -83,7 +83,6 @@
      (org-roam-db-location . "~/Obsidian/CPP_LEARNING/.org-roam.db")
      (org-roam-directory . "~/Obsidian/CPP_LEARNING/")
      (org-roam-db-location . "~/CPP_LEARNING/.org-roam.db"))))
-
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
